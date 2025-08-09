7ca20bf8647903382e0d6654a99418cfa2d260d1
Abdulaziz Ali <variant.abdulaziz@gmail.com>
2025-08-09 14:45:40 +0300

    remove unused action

--- src/lib/safe-action.ts
+++ src/lib/safe-action.ts
@@ -1,4 +1,7 @@
-import { DEFAULT_SERVER_ERROR_MESSAGE, createSafeActionClient } from "next-safe-action";
+import {
+  DEFAULT_SERVER_ERROR_MESSAGE,
+  createSafeActionClient,
+} from "next-safe-action";
 import axios from "axios";
 
 import { getSession } from "@/lib/session";
@@ -39,56 +42,4 @@ export const authenticatedAction = createSafeActionClient({
     (error) => Promise.reject(error)
   );
   return next({ ctx: { session: session, api } });
-});
-
-export const lichessAuthenticatedAction = createSafeActionClient({
-  handleServerError: (e) => {
-    if (e instanceof ActionError) {
-      return e.message;
-    }
-    return DEFAULT_SERVER_ERROR_MESSAGE;
-  },
-}).use(async ({ next }) => {
-  const lichessApi = axios.create({
-    baseURL: "https://lichess.org/api",
-    headers: {
-      "Content-type": "application/json",
-      withCredentials: true,
-    },
-  });
-
-  const api = axios.create({
-    baseURL: API_BASE_URL,
-    headers: {
-      "Content-type": "application/json",
-      withCredentials: true,
-    },
-  });
-  const session = await getSession();
-  if (!session) {
-    throw new ActionError("User is not authenticated");
-  }
-  if (!session.lichess || !session.lichess.access) {
-    redirect("/connect-lichess");
-  }
-  lichessApi.interceptors.request.use(
-    async (config: any) => {
-      if (session.access) {
-        config.headers["Authorization"] = `Bearer ${session.lichess?.access}`;
-      }
-      return config;
-    },
-    (error) => Promise.reject(error)
-  );
-
-  api.interceptors.request.use(
-    async (config: any) => {
-      if (session.access) {
-        config.headers["Authorization"] = `Bearer ${session.access}`;
-      }
-      return config;
-    },
-    (error) => Promise.reject(error)
-  );
-  return next({ ctx: { session: session, lichessApi, api } });
 });
